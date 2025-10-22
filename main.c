940ba0821e98ffc6d86168549c952da3e0b2ecd3
markian001 <markmurithi284@gmail.com>
2025-10-22 12:05:27 +0300

    Add files via upload

---
+++ main.c
@@ -0,0 +1,39 @@
+/*
+Name:Mark Ian Murithi
+Reg No:CT101/G/26541/25
+Description:A program that calculates electric bills
+*/
+
+#include <stdio.h>
+
+float calculateElectricBill(int units)
+{
+    float bill = 0;
+
+    if(units <= 100){
+       bill = units * 10;
+    }
+    else if(units <= 200){
+        bill = (100 * 10) + ((units - 100) * 15);
+    }
+    else {
+        bill = (100 * 10) + (100 * 15) + ((units - 200) * 20);
+    }
+
+    return bill;
+}
+
+int main()
+{
+    int units;
+    float totalBill;
+
+    printf("Enter Number of Units consumed: ");
+    scanf("%d", &units);
+
+    totalBill = calculateElectricBill(units);
+
+    printf("Total Bill is:KSH. %.2f\n", totalBill);
+
+    return 0;
+}
